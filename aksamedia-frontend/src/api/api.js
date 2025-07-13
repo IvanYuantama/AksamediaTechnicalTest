45729bdbff70fa94519d58cdcfb53383ada40e75
IvanYuantama <123520791+IvanYuantama@users.noreply.github.com>
2025-07-13 21:06:55 +0700

    Update api.js

--- aksamedia-frontend/src/api/api.js
+++ aksamedia-frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://aksamedia-ivan-backend.vercel.app/api/api";
+const API_URL = import.meta.env.BACKEND_URL;
 
 const api = axios.create({
   baseURL: API_URL,
